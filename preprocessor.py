ae065d6a9e2400b9fc09110375c48bc335e84577
MordovinNik <mordovin_nikolya@mail.ru>
2023-03-21 01:31:25 +0300

    added formatted output

--- preprocessor.py
+++ preprocessor.py
@@ -5,23 +5,38 @@ from pyparsing import nestedExpr
 from typing import List
 
 MACROS_DEFINE = "\\newcommand"
-macroses:List[Macros] = list()
+macroses: List[Macros] = list()
 
 
 def parse(src_file_path: str):
     lines = []
+    final_text = []
     with open(src_file_path, "r") as file:
         for line in file:
             lines.append(line)
 
     for line in lines:
         line = remove_comment(line)
+        line = re.sub("\n", "",line)
         if line.startswith(MACROS_DEFINE):
             macroses.append(parse_macros(line))
-        elif line.startswith(tuple([macros.name for macros in macroses])):
-            handle_macros(line)
-        else:
-            print(line)
+            continue
+
+        if any(macros.name in line for macros in macroses):
+            line = handle_macros(line)
+
+        if line not in ['\n', '\r\n', '']:
+            final_text.append(line)
+    print("\n")
+    print("Записаны макросы:")
+    for macros in macroses:
+        print(macros.name)
+    print('')
+
+    print("Обработанный текст:\n")
+    for line in final_text:
+        print(line)
+
 
 
 def remove_comment(text: str):
@@ -58,11 +73,12 @@ def parse_macros(line) -> Macros:
 
 def handle_macros(line: str):
     for macro in macroses:
-        if line.startswith(macro.name):
-            argument = re.search('\w+{(.+?)}', line).group(1)
-            for i in range(macro.args_count):
-                find_arg = "#" + str(i + 1)
-                # TODO заменить в структуре макроса  на аргумент argument
-                #line.replace(find_arg, argument)
-                #print(line)
-
+        if macro.name in line:
+            line = re.sub(f"\\\{macro.name[1:]}", "{"+f"НАЙДЕН МАКРОС:\\\{macro.name[1:]}"+"}", line)
+            return line
+            # argument = re.search('\w+{(.+?)}', line).group(1)
+            # for i in range(macro.args_count):
+            #     find_arg = "#" + str(i + 1)
+            #     # TODO заменить в структуре макроса  на аргумент argument
+            #     # line.replace(find_arg, argument)
+            #     # print(line)
