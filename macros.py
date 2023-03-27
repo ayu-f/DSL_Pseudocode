ebb05c9a6d8f2b52a501790053556f8fac9aba04
Dev <aalekseyf2001@mail.ru>
2023-03-27 21:33:53 +0300

    complete preprocessor

--- macros.py
+++ macros.py
@@ -1,7 +1,12 @@
+import re
+from pyparsing import nestedExpr
 
 
 class Macros:
+    body_str = ""
     args_count = int(0)
     name = str("")
     args = []
-    body = []
+
+    def get_body(self):
+        return nestedExpr('{', '}').parseString(self.body_str).asList()
