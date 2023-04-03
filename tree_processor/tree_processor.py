87caa7dad219c9cebc9a625f011bb807a0cee17d
Dev <aalekseyf2001@mail.ru>
2023-04-03 21:21:25 +0300

    fix root dir

--- tree_processor/tree_processor.py
+++ tree_processor/tree_processor.py
@@ -3,7 +3,7 @@ from print_node import print_node
 from unformated_nonterm_print_map import unformated_nonterm_print_map
 
 
-file = open("../test_tree.json", "r", encoding="UTF-8")
+file = open("test_tree.json", "r", encoding="UTF-8")
 code = file.read()
 res = json.loads(code)
 
